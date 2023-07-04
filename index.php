b3e0187c90a952e2492238216fa69b4bf84f6d7a
Quintusdev <quintusdev92@gmail.com>
2023-07-04 14:51:33 +0200

    ES. BASE COMPLETATO

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
 <?php
+    $hotels = [
 
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
 
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -12,13 +49,52 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <!-- link Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <!-- link CSS -->
+        <link rel="stylesheet" href="./css/style.css">
         <!-- titolo scheda browser -->
         <title>Hotel PHP</title>
     </head>
 
     <body>
         <div class="container">
-        
+            <div class="row">
+                <div class="col-12">
+                    <!-- Inserisco la tabella dove visualizzo i dati completi -->
+                <table class="table table-hover mt-5">
+                    <thead class="text-center">
+                        <tr class="table-dark">
+                            <th scope="col">NAME</th>
+                            <th scope="col">DESCRIPTION</th>
+                            <th scope="col">PARKING</th>
+                            <th scope="col">VOTE</th>
+                            <th scope="col">DISTANCE TO CENTER</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($hotels as $hotel) { ?>
+                            <tr class="text-center table-warning">
+                                <th scope="row">
+                                    <?php echo $hotel['name']; ?>
+                                </th>
+                                <td> 
+                                    <?php echo $hotel['description']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['parking']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['vote']; ?>
+                                </td>
+                                <td>
+                                    <!-- Aggiunta dicitura KM dopo la distanza -->
+                                    <?php echo $hotel['distance_to_center']." km";?>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+                </div>
+            </div>
         </div>
     </body>
 </html>
